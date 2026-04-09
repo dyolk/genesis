09b7ee114899db774891ee315014668b7ff11bf4
Mr.KiNG <hi@hang.gold>
2026-04-09 22:22:04 +0800

    :sparkles: feat(web-demo): 构建简单的 web-demo

--- devops/app/web-demo/src/main.rs
+++ devops/app/web-demo/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json,
+    response::IntoResponse,
+    http::StatusCode,
+    Router,
+    routing::get,
+    extract::State,
+};
+use serde::{Serialize, Deserialize};
+use std::net::{IpAddr, Ipv4Addr};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+#[derive(Clone)]
+struct AppState {
+    healthy: Arc<AtomicBool>,
 }
+
+#[derive(Serialize, Deserialize)]
+struct InfoResponse {
+    ip: String,
+    hostname: String,
+    local_ip: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct HealthResponse {
+    status: String,
+    message: String,
+}
+
+async fn get_info() -> impl InfoResponse {}
